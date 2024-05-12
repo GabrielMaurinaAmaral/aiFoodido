309af620aecaf88cca009f2e44542bb83fc28446
GabrielMaurinaAmaral <gabrielmaurina2003@gmail.com>
2024-05-12 10:31:49 -0300

    inicio da tela do pedido

--- src/components/ProductListItem.tsx
+++ src/components/ProductListItem.tsx
@@ -10,7 +10,7 @@ type ProductListItemProps = {
 export default function ProductListItem({ product }: { product: Product }) {
     return (
         
-        <Link href={`/${product.id}`} asChild>
+        <Link href={`menu/${product.id}`} asChild>
             <Pressable style={styles.container}>
                 <Image source={{ uri: product.image || 'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/food/default.png' }}
                     style={styles.image}
